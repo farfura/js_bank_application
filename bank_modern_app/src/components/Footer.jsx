e1630b99b46d44064334c2d59276edd769baff34
fareeha nadeem <fareehanadeem.new@gmail.com>
2023-08-10 15:12:31 +0500

    Your commit message here

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "../style";
 import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
@@ -5,32 +6,34 @@ import { footerLinks, socialMedia } from "../constants";
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex-[1] flex flex-col justify-start mr-10">
+      <div className="flex-1 flex flex-col justify-start mr-10">
         <img
           src={logo}
           alt="hoobank"
-          className="w-[266px] h-[72.14px] object-contain"
+          className="w-[266px] h-[72px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+        <p
+          className={`${styles.paragraph} mt-4 max-w-[310px]`}
+        >
+          A new way to make payments easy, reliable, and secure.
         </p>
       </div>
-
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+        {footerLinks.map((footerLink) => (
+          <div
+            key={footerLink.key}
+            className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+          >
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerlink.title}
+              {footerLink.title}
             </h4>
             <ul className="list-none mt-4">
-              {footerlink.links.map((link, index) => (
+              {footerLink.links.map((Link, index) => (  // Added 'index' parameter
                 <li
-                  key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
+                  key={Link.name}
+                  className={`font-poppins font-normal text-[15px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length -1 ? 'mb-4' : 'mb-0'}`}
                 >
-                  {link.name}
+                  {Link.name}
                 </li>
               ))}
             </ul>
@@ -38,24 +41,22 @@ const Footer = () => (
         ))}
       </div>
     </div>
-
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
-      </p>
-
+      <p className="font-poppins font-normal  text-center text-[18px] leading-[27px] text-white">2023 Fareeha Nadeem. All Rights Reserved.</p>
       <div className="flex flex-row md:mt-0 mt-6">
-        {socialMedia.map((social, index) => (
+        {socialMedia.map((social,index)=>(
           <img
-            key={social.id}
-            src={social.icon}
-            alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
-            onClick={() => window.open(social.link)}
-          />
+          key={social.id}
+          src={social.icon}
+          alt={social.id}
+          className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+            index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+          }`}
+          onClick={() => window.open(social.link)}
+        />
+          
         ))}
+
       </div>
     </div>
   </section>
